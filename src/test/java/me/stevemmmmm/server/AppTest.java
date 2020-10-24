79a98eb6d5945772c60215ae16d46651f2ea5b6d
Stevemmmmm <estebangarcia2121@gmail.com>
2020-10-24 00:39:20 -0700

    Initial commit

---
+++ src/test/java/me/stevemmmmm/server/AppTest.java
@@ -0,0 +1,20 @@
+package me.stevemmmmm.server;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
