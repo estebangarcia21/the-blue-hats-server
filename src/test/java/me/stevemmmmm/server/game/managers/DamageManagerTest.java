cabe16831851cd20f3ec3593fcffc3c7212ed437
Stevemmmmm <estebangarcia2121@gmail.com>
2020-10-24 19:48:40 -0700

    Added unit tests for player and arrow is canceled event methods

--- src/test/java/me/stevemmmmm/server/game/managers/DamageManagerTest.java
+++ src/test/java/me/stevemmmmm/server/game/managers/DamageManagerTest.java
@@ -1,5 +1,26 @@
 package me.stevemmmmm.server.game.managers;
 
-public class DamageManagerTest {
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Player;
+import org.junit.Test;
+
+public class DamageManagerTest {
+    @Test
+    public void testPlayerIsInCanceledEvent() {
+        DamageManager manager = new DamageManager();
+        Player arrow = mock(Player.class);
+
+        assertEquals(false, manager.playerIsInCanceledEvent(arrow));
+    }
+
+    @Test
+    public void testArrowIsInCanceledEvent() {
+        DamageManager manager = new DamageManager();
+        Arrow arrow = mock(Arrow.class);
+
+        assertEquals(false, manager.arrowIsInCanceledEvent(arrow));
+    }
 }
