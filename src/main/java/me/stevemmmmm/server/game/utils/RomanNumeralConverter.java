abb1114878657fa72c78705d9c44d99164e53d73
Stevemmmmm <estebangarcia2121@gmail.com>
2020-10-25 12:00:48 -0700

    Created Roman Numeral Converter utility class

---
+++ src/main/java/me/stevemmmmm/server/game/utils/RomanNumeralConverter.java
@@ -0,0 +1,87 @@
+package me.stevemmmmm.server.game.utils;
+
+public class RomanNumeralConverter {
+    public String convertToRomanNumeral(int value) {
+        switch (value) {
+            case 0:
+                return "None";
+            case 1:
+                return "I";
+            case 2:
+                return "II";
+            case 3:
+                return "III";
+            case 4:
+                return "IV";
+            case 5:
+                return "V";
+            case 6:
+                return "VI";
+            case 7:
+                return "VII";
+            case 8:
+                return "VIII";
+            case 9:
+                return "IX";
+            case 10:
+                return "X";
+            case 11:
+                return "XI";
+            case 12:
+                return "XII";
+            case 13:
+                return "XIII";
+            case 14:
+                return "XIV";
+            case 15:
+                return "XV";
+            case 16:
+                return "XVI";
+            case 17:
+                return "XVII";
+            case 18:
+                return "XVIII";
+            case 19:
+                return "XIX";
+            case 20:
+                return "XX";
+            case 21:
+                return "XXI";
+            case 22:
+                return "XXII";
+            case 23:
+                return "XXIII";
+            case 24:
+                return "XXIV";
+            case 25:
+                return "XXV";
+            case 26:
+                return "XXVI";
+            case 27:
+                return "XXVII";
+            case 28:
+                return "XXVIII";
+            case 29:
+                return "XXIX";
+            case 30:
+                return "XXX";
+            case 35:
+                return "XXXV";
+        }
+
+        return null;
+    }
+
+    public int convertRomanNumeralToInteger(String numeral) {
+        switch (numeral) {
+            case "I":
+                return 1;
+            case "II":
+                return 2;
+            case "III":
+                return 3;
+        }
+
+        return -1;
+    }
+}
