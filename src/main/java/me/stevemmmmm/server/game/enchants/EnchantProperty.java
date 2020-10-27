aa7e153f62e1ca9658086077f20762d7750fec03
Stevemmmmm <estebangarcia2121@gmail.com>
2020-10-26 18:24:55 -0700

    Added max args check for constructor args

--- src/main/java/me/stevemmmmm/server/game/enchants/EnchantProperty.java
+++ src/main/java/me/stevemmmmm/server/game/enchants/EnchantProperty.java
@@ -1,10 +1,13 @@
 package me.stevemmmmm.server.game.enchants;
 
 public class EnchantProperty<T> {
-    private final T[] values;
+    private T[] values;
 
     @SafeVarargs
     public EnchantProperty(T... values) {
+        if (values.length > 3)
+            throw new IllegalArgumentException("Enchants only have 3 levels. You had " + values.length + "properties.");
+
         this.values = values;
     }
 
