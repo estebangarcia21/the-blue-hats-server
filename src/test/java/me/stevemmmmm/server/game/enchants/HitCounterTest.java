adfd065a8292f074648fd37f852c18865683d46b
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-10-29 22:52:21 -0700

    Properly implemented all tests

--- src/test/java/me/stevemmmmm/server/game/enchants/HitCounterTest.java
+++ src/test/java/me/stevemmmmm/server/game/enchants/HitCounterTest.java
@@ -1,17 +1,26 @@
 package me.stevemmmmm.server.game.enchants;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitScheduler;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import me.stevemmmmm.server.core.Main;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ Bukkit.class })
 public class HitCounterTest {
     @Mock
     private Player player;
@@ -21,37 +30,55 @@ public class HitCounterTest {
     @Before
     public void setup() {
         MockitoAnnotations.openMocks(this);
+        PowerMockito.mockStatic(Bukkit.class);
 
-        counter = new HitCounter(mock(Main.class));
+        counter = spy(new HitCounter(mock(Main.class)));
     }
 
-    @Test(expected = NullPointerException.class)
-    public void testAddOne() {
+    @Test
+    public void AddOne() {
+        doNothing().when(counter).startHitResetTimer(player);
+
         counter.addOne(player);
 
         assertTrue(counter.hasHits(player, 1));
     }
 
-    @Test(expected = NullPointerException.class)
-    public void testAdd() {
+    @Test
+    public void Add() {
+        doNothing().when(counter).startHitResetTimer(player);
+
         counter.add(player, 5);
 
         assertTrue(counter.hasHits(player, 5));
     }
 
     @Test
-    public void testHasRequiredHits() {
-        assertTrue(counter.hasHits(player, 0) && !counter.hasHits(player, 1));
+    public void HasRequiredHits() {
+        doNothing().when(counter).startHitResetTimer(player);
+
+        assertTrue(counter.hasHits(player, 0));
+        assertFalse(counter.hasHits(player, 1));
     }
 
-    @Test(expected = NullPointerException.class)
-    public void testHitResetTimer() {
+    @Test
+    public void HitResetTimer() {
+        doNothing().when(counter).startHitResetTimer(player);
+
         counter.add(player, 5);
 
-        assertTrue(counter.hasHits(player, 5));
+        doCallRealMethod().when(counter).startHitResetTimer(player);
+
+        Server server = mock(Server.class);
+        BukkitScheduler scheduler = mock(BukkitScheduler.class);
+
+        when(Bukkit.getServer()).thenReturn(server);
+        when(server.getScheduler()).thenReturn(scheduler);
+
+        when(Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(any(), any(Runnable.class), anyLong(), anyLong())).thenReturn(0);
 
         counter.startHitResetTimer(player);
 
-        assertTrue(counter.hasHits(player, 0) && !counter.hasHits(player, 1));
+        assertTrue(counter.hasHits(player, 0));
     }
 }
