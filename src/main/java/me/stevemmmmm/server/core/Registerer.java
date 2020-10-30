05b955f77eb19cd2994e893a951bf6dc0f0d5eb5
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-10-29 23:02:36 -0700

    Reformatted Registerer.java

--- src/main/java/me/stevemmmmm/server/core/Registerer.java
+++ src/main/java/me/stevemmmmm/server/core/Registerer.java
@@ -4,11 +4,9 @@ import me.stevemmmmm.server.game.enchants.CustomEnchantManager;
 import me.stevemmmmm.server.game.managers.*;
 
 public interface Registerer {
-    void registerEnchants(DamageManager damageManager, BowManager bowManager,
-            CustomEnchantManager customEnchantManager);
+    void registerEnchants(DamageManager damageManager, BowManager bowManager, CustomEnchantManager customEnchantManager);
 
-    void registerPerks(DamageManager damageManager, BowManager bowManager,
-            CustomEnchantManager customEnchantManager);
+    void registerPerks(DamageManager damageManager, BowManager bowManager, CustomEnchantManager customEnchantManager);
 
     void registerCommands(Main main, DamageManager damageManager, CombatManager combatManager, GrindingSystem grindingSystem, CustomEnchantManager customEnchantManager, WorldSelectionManager worldSelectionManager);
 }
