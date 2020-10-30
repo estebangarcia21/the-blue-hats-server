16d55b376cf1b8874d4089d8eff01aab723e2645
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-10-30 01:26:11 -0700

    Setup basic setup method

---
+++ src/test/java/me/stevemmmmm/server/game/utils/LoreBuilderTest.java
@@ -0,0 +1,21 @@
+package me.stevemmmmm.server.game.utils;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoreBuilderTest {
+    private LoreBuilder loreBuilder;
+
+    private ArrayList<String> description;
+    private List<String[]> parameters;
+
+    @Before
+    public void setup() {
+        // TODO Initialize fields by reflection
+
+        loreBuilder = new LoreBuilder();
+    }
+}
