0102240ba5f4c87c7f500aa7e6f84f1e841348a5
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-10-31 15:29:33 -0700

    Condensed register methods to a single method

--- src/main/java/me/stevemmmmm/server/core/Registerer.java
+++ src/main/java/me/stevemmmmm/server/core/Registerer.java
@@ -4,9 +4,8 @@ import me.stevemmmmm.server.game.enchants.CustomEnchantManager;
 import me.stevemmmmm.server.game.managers.*;
 
 public interface Registerer {
-    void registerEnchants(DamageManager damageManager, BowManager bowManager, CustomEnchantManager customEnchantManager);
-
-    void registerPerks(DamageManager damageManager, BowManager bowManager, CustomEnchantManager customEnchantManager);
-
-    void registerCommands(Main main, DamageManager damageManager, CombatManager combatManager, GrindingSystem grindingSystem, CustomEnchantManager customEnchantManager, WorldSelectionManager worldSelectionManager);
+    void registerGameLogic(Main main, DamageManager damageManager, CombatManager combatManager,
+                           BowManager bowManager, GrindingSystem grindingSystem,
+                           CustomEnchantManager customEnchantManager,
+                           WorldSelectionManager worldSelectionManager);
 }
