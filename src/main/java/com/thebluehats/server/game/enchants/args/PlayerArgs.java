344bd0108709d14f054503b67cbf8145a7051e01
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-08 20:13:31 -0800

    Renamed to PlayerArgs.java

--- src/main/java/com/thebluehats/server/game/enchants/args/PlayerArgs.java
+++ src/main/java/com/thebluehats/server/game/enchants/args/PlayerArgs.java
@@ -2,10 +2,10 @@ package com.thebluehats.server.game.enchants.args;
 
 import org.bukkit.entity.Player;
 
-public class PlayerEventArgs {
+public class PlayerArgs {
     private final Player player;
 
-    public PlayerEventArgs(Player player) {
+    public PlayerArgs(Player player) {
         this.player = player;
     }
 
