e6e8e5bc263b582bf7e5f326757145f637bdc31f
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-08 20:08:46 -0800

    Added dependencies

--- src/main/java/com/thebluehats/server/core/modules/DamageManagerModule.java
+++ src/main/java/com/thebluehats/server/core/modules/DamageManagerModule.java
@@ -3,6 +3,8 @@ package com.thebluehats.server.core.modules;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import com.thebluehats.server.core.modules.annotations.MirrorReference;
+import com.thebluehats.server.game.enchants.Mirror;
 import com.thebluehats.server.game.managers.combat.CombatManager;
 import com.thebluehats.server.game.managers.combat.DamageManager;
 import com.thebluehats.server.game.managers.enchants.CustomEnchantManager;
@@ -10,8 +12,9 @@ import com.thebluehats.server.game.managers.enchants.CustomEnchantManager;
 public class DamageManagerModule extends AbstractModule {
     @Provides
     @Singleton
-    static DamageManager provideDamageManager(CustomEnchantManager customEnchantManager, CombatManager combatManager) {
-        return new DamageManager(customEnchantManager, combatManager);
+    static DamageManager provideDamageManager(@MirrorReference Mirror mirror, CustomEnchantManager customEnchantManager,
+            CombatManager combatManager) {
+        return new DamageManager(mirror, customEnchantManager, combatManager);
     }
 
     @Override
--- src/main/java/com/thebluehats/server/game/managers/combat/DamageManager.java
+++ src/main/java/com/thebluehats/server/game/managers/combat/DamageManager.java
@@ -5,189 +5,184 @@ import java.util.HashMap;
 import java.util.UUID;
 
 import com.google.inject.Inject;
+import com.thebluehats.server.core.modules.annotations.MirrorReference;
 import com.thebluehats.server.game.enchants.Mirror;
 import com.thebluehats.server.game.managers.enchants.CustomEnchantManager;
+import com.thebluehats.server.game.managers.enchants.EnchantProperty;
 import com.thebluehats.server.game.managers.game.RegionManager;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.projectiles.ProjectileSource;
 
 public class DamageManager {
     private final HashMap<UUID, EventData> eventData = new HashMap<>();
     private final ArrayList<UUID> canceledPlayers = new ArrayList<>();
-//    private final Mirror mirror = new Mirror(null);
+    private final ArrayList<UUID> removeCriticalDamage = new ArrayList<>();
+
+    private final Mirror mirror;
+    private final EnchantProperty<Float> mirrorReflectionValues = new EnchantProperty<>(0f, .25f, .5f);
 
     private final CustomEnchantManager customEnchantManager;
     private final CombatManager combatManager;
 
     @Inject
-    public DamageManager(CustomEnchantManager customEnchantManager, CombatManager combatManager) {
+    public DamageManager(@MirrorReference Mirror mirror, CustomEnchantManager customEnchantManager,
+            CombatManager combatManager) {
         this.customEnchantManager = customEnchantManager;
         this.combatManager = combatManager;
+        this.mirror = mirror;
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onHit(EntityDamageByEntityEvent event) {
+        UUID damagerUuid = event.getDamager().getUniqueId();
+
+        if (eventData.containsKey(damagerUuid))
+            return;
+
+        if (canceledPlayers.contains(damagerUuid)) {
+            event.setCancelled(true);
+        } else {
+            event.setDamage(getDamageFromEvent(event));
+        }
+
+        eventData.remove(damagerUuid);
+        canceledPlayers.remove(damagerUuid);
+    }
+
+    public double getDamageFromEvent(EntityDamageByEntityEvent event) {
+        return calculateDamage(event.getDamage(), event);
+    }
+
+    public double getFinalDamageFromEvent(EntityDamageByEntityEvent event) {
+        return calculateDamage(event.getFinalDamage(), event);
+    }
+
+    public void addDamage(EntityDamageByEntityEvent event, double value, CalculationMode mode) {
+        EventData data = eventData.get(event.getDamager().getUniqueId());
+
+        if (mode == CalculationMode.ADDITIVE) {
+            data.setAdditiveDamage(data.getAdditiveDamage() + value);
+        }
+
+        if (mode == CalculationMode.MULTIPLICATIVE) {
+            data.setMultiplicativeDamage(data.getMultiplicativeDamage() + value);
+        }
+    }
+
+    public void reduceDamage(EntityDamageByEntityEvent event, double value) {
+        EventData data = eventData.get(event.getDamager().getUniqueId());
+
+        if (data.getReductionAmount() == 1) {
+            data.setReductionAmount(data.getReductionAmount() - (1 - value));
+
+            return;
+        }
+
+        data.setReductionAmount(1 - data.getReductionAmount() * value);
+    }
+
+    public void reduceAbsoluteDamage(EntityDamageByEntityEvent event, double value) {
+        EventData data = eventData.get(event.getDamager().getUniqueId());
+
+        data.setAbsoluteReductionAmount(data.getAbsoluteReductionAmount() + value);
+    }
+
+    public void removeExtraCriticalDamage(EntityDamageByEntityEvent event) {
+        removeCriticalDamage.add(event.getDamager().getUniqueId());
+    }
+
+    public void setEventAsCanceled(EntityDamageByEntityEvent event) {
+        canceledPlayers.add(event.getDamager().getUniqueId());
+    }
+
+    public boolean eventIsNotCancelled(EntityDamageByEntityEvent event) {
+        return !canceledPlayers.contains(event.getDamager().getUniqueId());
     }
 
     public boolean playerIsInCanceledEvent(Player player) {
-        return canceledPlayers.contains(player.getUniqueId());
-    }
+        for (UUID uuid : canceledPlayers) {
+            Entity entity = Bukkit.getEntity(uuid);
 
-    public boolean arrowIsInCanceledEvent(Arrow arrow) {
-        if (arrow.getShooter() instanceof Player)
-            return canceledPlayers.contains(((Player) arrow.getShooter()).getUniqueId());
+            if (entity instanceof Projectile) {
+                ProjectileSource projectileShooter = ((Projectile) entity).getShooter();
+
+                if (projectileShooter instanceof Player) {
+                    if (projectileShooter.equals(player)) {
+                        return true;
+                    }
+                }
+            }
+
+            if (entity instanceof Player) {
+                if (entity.equals(player)) {
+                    return true;
+                }
+            }
+        }
 
         return false;
     }
 
-    // @EventHandler(priority = EventPriority.HIGHEST)
-    // public void onHit(EntityDamageByEntityEvent event) {
-    // if (!eventData.containsKey(event)) {
-    // eventData.put(event, new EventData());
-    // }
+    public boolean arrowIsInCanceledEvent(Arrow projectile) {
+        for (UUID uuid : canceledPlayers) {
+            Entity entity = Bukkit.getEntity(uuid);
 
-    // if (canceledEvents.contains(event)) {
-    // event.setCancelled(true);
-    // } else {
-    // event.setDamage(getDamageFromEvent(event));
-    // }
+            if (entity instanceof Arrow) {
+                if (entity.equals(projectile)) {
+                    return true;
+                }
+            }
+        }
 
-    // eventData.remove(event);
-    // canceledEvents.remove(event);
-    // removeCriticalDamage.remove(event);
-    // }
+        return false;
+    }
 
-    // public double getDamageFromEvent(EntityDamageByEntityEvent event) {
-    // return calculateDamage(event.getDamage(), event);
-    // }
+    public void doTrueDamage(Player target, double damage) {
+        if (mirror.itemHasEnchant(target.getInventory().getLeggings())) {
+            target.setHealth(Math.max(0, target.getHealth() - damage));
+            target.damage(0);
+        }
+    }
 
-    // public double getFinalDamageFromEvent(EntityDamageByEntityEvent event) {
-    // return calculateDamage(event.getFinalDamage(), event);
-    // }
+    public void doTrueDamage(Player target, double damage, Player reflectTo) {
+        int level = mirror.getEnchantLevel(target.getInventory().getLeggings());
 
-    // public void addDamage(EntityDamageByEntityEvent event, double value,
-    // CalculationMode mode) {
-    // EventData data = EventData.fromEvent(event);
+        combatManager.combatTag(target);
 
-    // if (mode == CalculationMode.ADDITIVE) {
-    // data.setAdditiveDamage(data.getAdditiveDamage() + value);
-    // }
+        if (!mirror.itemHasEnchant(target.getInventory().getLeggings())) {
+            if (target.getHealth() - damage < 0) {
+                safeSetPlayerHealth(target, 0);
+            } else {
+                target.damage(0);
+                safeSetPlayerHealth(target, target.getHealth() - damage);
+            }
+        } else if (level != 1) {
+            try {
+                if (reflectTo.getHealth() - (damage * mirrorReflectionValues.getValueAtLevel(level)) < 0) {
+                    safeSetPlayerHealth(target, 0);
+                } else {
+                    reflectTo.damage(0);
 
-    // if (mode == CalculationMode.MULTIPLICATIVE) {
-    // data.setMultiplicativeDamage(data.getMultiplicativeDamage() + value);
-    // }
-    // }
+                    combatManager.combatTag(target);
 
-    // public void reduceDamage(EntityDamageByEntityEvent event, double value) {
-    // EventData data = EventData.fromEvent(event);
-
-    // if (data.getReductionAmount() == 1) {
-    // data.setReductionAmount(data.getReductionAmount() - (1 - value));
-    // return;
-    // }
-
-    // data.setReductionAmount(1 - data.getReductionAmount() * value);
-    // }
-
-    // public void reduceAbsoluteDamage(EntityDamageByEntityEvent event, double
-    // value) {
-    // EventData data = EventData.fromEvent(event);
-
-    // data.setAbsoluteReductionAmount(data.getAbsoluteReductionAmount() + value);
-    // }
-
-    // public void removeExtraCriticalDamage(EntityDamageByEntityEvent event) {
-    // removeCriticalDamage.add(event);
-    // }
-
-    // public void setEventAsCanceled(EntityDamageByEntityEvent event) {
-    // canceledEvents.add(event);
-    // }
-
-    // public boolean isEventNotCancelled(EntityDamageByEntityEvent event) {
-    // return !canceledEvents.contains(event);
-    // }
-
-    // public boolean playerIsInCanceledEvent(Player player) {
-    // for (EntityDamageByEntityEvent event : canceledEvents) {
-    // if (event.getDamager() instanceof Projectile) {
-    // if (((Projectile) event.getDamager()).getShooter() instanceof Player) {
-    // if (((Projectile) event.getDamager()).getShooter().equals(player)) {
-    // return true;
-    // }
-    // }
-    // }
-
-    // if (event.getDamager() instanceof Player) {
-    // if (event.getDamager().equals(player)) {
-    // return true;
-    // }
-    // }
-
-    // if (event.getEntity() instanceof Player) {
-    // if (event.getEntity().equals(player)) {
-    // return true;
-    // }
-    // }
-    // }
-
-    // return false;
-    // }
-
-    // public boolean arrowIsInCanceledEvent(Arrow projectile) {
-    // for (EntityDamageByEntityEvent event : canceledEvents) {
-    // if (event.getDamager() instanceof Arrow) {
-    // if (event.getDamager().equals(projectile)) {
-    // return true;
-    // }
-    // }
-
-    // if (event.getEntity() instanceof Arrow) {
-    // if (event.getEntity().equals(projectile)) {
-    // return true;
-    // }
-    // }
-    // }
-
-    // return false;
-    // }
-
-//    public void doTrueDamage(Player target, double damage) {
-//        if (mirror.itemHasEnchant(target.getInventory().getLeggings())) {
-//            target.setHealth(Math.max(0, target.getHealth() - damage));
-//            target.damage(0);
-//        }
-//    }
-//
-//    public void doTrueDamage(Player target, double damage, Player reflectTo) {
-//        int level = mirror.getEnchantLevel(target.getInventory().getLeggings());
-//
-//        combatManager.combatTag(target);
-//
-//        if (!mirror.itemHasEnchant(target.getInventory().getLeggings())) {
-//            if (target.getHealth() - damage < 0) {
-//                safeSetPlayerHealth(target, 0);
-//            } else {
-//                target.damage(0);
-//                safeSetPlayerHealth(target, target.getHealth() - damage);
-//            }
-//        } else if (level != 1) {
-//            try {
-//                if (reflectTo.getHealth() - (damage * mirror.DAMAGE_REFLECTION.getValueAtLevel(level)) < 0) {
-//                    safeSetPlayerHealth(target, 0);
-//                } else {
-//                    reflectTo.damage(0);
-//
-//                    combatManager.combatTag(target);
-//
-//                    safeSetPlayerHealth(reflectTo, Math.max(0,
-//                            reflectTo.getHealth() - (damage * mirror.DAMAGE_REFLECTION.getValueAtLevel(level))));
-//                }
-//            } catch (NullPointerException ignored) {
-//
-//            }
-//        }
-//    }
+                    safeSetPlayerHealth(reflectTo, Math.max(0,
+                            reflectTo.getHealth() - (damage * mirrorReflectionValues.getValueAtLevel(level))));
+                }
+            } catch (NullPointerException ignored) {
+            }
+        }
+    }
 
     public void safeSetPlayerHealth(Player player, double health) {
         if (!RegionManager.getInstance().playerIsInRegion(player, RegionManager.RegionType.SPAWN)) {
@@ -206,41 +201,33 @@ public class DamageManager {
                 && !player.hasPotionEffect(PotionEffectType.BLINDNESS);
     }
 
-    // private double calculateDamage(double initialDamage,
-    // EntityDamageByEntityEvent event) {
-    // EventData data = EventData.fromEvent(event);
+    private double calculateDamage(double initialDamage, EntityDamageByEntityEvent event) {
+        EventData data = eventData.get(event.getDamager().getUniqueId());
 
-    // double damage = initialDamage * data.getAdditiveDamage() *
-    // data.getMultiplicativeDamage()
-    // * data.getReductionAmount() - data.getAbsoluteReductionAmount();
+        double damage = initialDamage * data.getAdditiveDamage() * data.getMultiplicativeDamage()
+                * data.getReductionAmount() - data.getAbsoluteReductionAmount();
 
-    // if (removeCriticalDamage.contains(event)) {
-    // damage *= .667;
-    // }
+        if (removeCriticalDamage.contains(event.getDamager().getUniqueId())) {
+            damage *= .667;
+        }
 
-    // if (event.getEntity() instanceof Player) {
-    // Player player = (Player) event.getEntity();
+        if (event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
 
-    // if (player.getInventory().getLeggings() != null) {
-    // if (player.getInventory().getLeggings().getType() ==
-    // Material.LEATHER_LEGGINGS) {
-    // if
-    // (!customEnchantManager.getItemEnchants(player.getInventory().getLeggings()).isEmpty())
-    // {
-    // // TODO This is the only way to try to make leather pants to iron in 1.8.8
-    // // spigot. I need to upgrade to 1.9+ spigot to fix this. This heavily changes
-    // // the damage output
-    // damage *= 0.871;
-    // }
-    // }
-    // }
-    // }
+            if (player.getInventory().getLeggings() != null) {
+                if (player.getInventory().getLeggings().getType() == Material.LEATHER_LEGGINGS) {
+                    if (!customEnchantManager.getItemEnchants(player.getInventory().getLeggings()).isEmpty()) {
+                        damage *= 0.871;
+                    }
+                }
+            }
+        }
 
-    // if (damage <= 0)
-    // damage = 1;
+        if (damage <= 0)
+            damage = 1;
 
-    // return damage;
-    // }
+        return damage;
+    }
 }
 
 class EventData {
@@ -249,17 +236,6 @@ class EventData {
 
     private double reductionAmount = 1;
     private double absoluteReductionAmount;
-
-    private EventData() {
-    }
-
-    // public static EventData fromEvent(EntityDamageByEntityEvent event) {
-    // if (!eventData.containsKey(event)) {
-    // eventData.put(event, new EventData());
-    // }
-
-    // return eventData.get(event);
-    // }
 
     public double getAdditiveDamage() {
         return additiveDamage;
