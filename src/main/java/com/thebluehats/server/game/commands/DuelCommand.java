be2d236315268e766678ae7d563699eefd3d01cd
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-08 20:35:42 -0800

    Implemented CommandUtils

--- src/main/java/com/thebluehats/server/game/commands/DuelCommand.java
+++ src/main/java/com/thebluehats/server/game/commands/DuelCommand.java
@@ -1,7 +1,8 @@
 package com.thebluehats.server.game.commands;
 
+import com.thebluehats.server.game.utils.CommandUtils;
+
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,27 +11,27 @@ import org.bukkit.entity.Player;
 public class DuelCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        String cmdName = "duel";
+
         if (sender instanceof Player) {
             Player player = (Player) sender;
 
-            if (label.equalsIgnoreCase("duel")) {
+            if (label.equalsIgnoreCase(cmdName)) {
                 if (args.length == 0) {
-                    player.sendMessage(ChatColor.DARK_PURPLE + "Usage:" + ChatColor.RED + " /duel <player>");
+                    player.sendMessage(CommandUtils.formatMessage(cmdName, String.format("/%s <player>", cmdName)));
                 } else {
                     if (Bukkit.getPlayer(args[0]) == null) {
-                        player.sendMessage(
-                                ChatColor.DARK_PURPLE + "Error! " + ChatColor.RED + "This player is not online!");
+                        player.sendMessage(CommandUtils.formatMessage("Error!", "This player is not online!"));
+
                         return true;
                     }
 
                     if (player == Bukkit.getPlayer(args[0])) {
-                        player.sendMessage(
-                                ChatColor.DARK_PURPLE + "Error! " + ChatColor.RED + "You can not duel yourself!");
+                        player.sendMessage(CommandUtils.formatMessage("Error!", "You can not duel yourself!"));
                         return true;
                     }
 
-                    // TODO Dueling manager
-//                    DuelingManager.getInstance().startDuel(new Duel(player, Bukkit.getPlayer(args[0])));
+                    // TODO Call duel manager
                 }
             }
         }
