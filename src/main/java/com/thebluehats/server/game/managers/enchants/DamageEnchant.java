3937fe662f57ebf7a8da02e5da389264fddb8f71
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-13 18:28:40 -0800

    Renamed to PostEventTemplateResult

--- src/main/java/com/thebluehats/server/game/managers/enchants/DamageEnchant.java
+++ src/main/java/com/thebluehats/server/game/managers/enchants/DamageEnchant.java
@@ -1,11 +1,11 @@
 package com.thebluehats.server.game.managers.enchants;
 
-import com.thebluehats.server.game.enchants.processedevents.ProcessedEntityDamageByEntityEvent;
+import com.thebluehats.server.game.enchants.processedevents.PostEventTemplateResult;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
-public interface DamageEnchant extends CustomEnchant, PostEventExecutor<ProcessedEntityDamageByEntityEvent> {
+public interface DamageEnchant extends CustomEnchant, PostEventExecutor<PostEventTemplateResult> {
     @EventHandler
     void onEntityDamageEntity(EntityDamageByEntityEvent event);
 }
