a13efe445523698e6c85d4258713c42137be47b5
Stevemmmmmm <estebangarcia2121@gmail.com>
2020-12-13 18:29:54 -0800

    Removed uneeded dependencies

--- src/main/java/com/thebluehats/server/core/modules/MirrorModule.java
+++ src/main/java/com/thebluehats/server/core/modules/MirrorModule.java
@@ -3,17 +3,15 @@ package com.thebluehats.server.core.modules;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
-import com.thebluehats.server.core.modules.annotations.AllEventTemplates;
 import com.thebluehats.server.core.modules.annotations.MirrorReference;
 import com.thebluehats.server.game.enchants.Mirror;
-import com.thebluehats.server.game.managers.combat.templates.PostEventTemplate;
 
 public class MirrorModule extends AbstractModule {
     @Provides
     @Singleton
     @MirrorReference
-    static Mirror provideMirror(@AllEventTemplates PostEventTemplate[] templates) {
-        return new Mirror(templates);
+    static Mirror provideMirror() {
+        return new Mirror();
     }
 
     @Override
