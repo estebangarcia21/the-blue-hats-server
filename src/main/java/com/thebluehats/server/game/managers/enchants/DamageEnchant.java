7fdea8018919a7c7b3d8c9f743a7a40640397333
Im-Stevemmmmm <estebangarcia2121@gmail.com>
2020-12-20 18:21:43 -0800

    Removed uneeded @EventHandler annotation

--- src/main/java/com/thebluehats/server/game/managers/enchants/DamageEnchant.java
+++ src/main/java/com/thebluehats/server/game/managers/enchants/DamageEnchant.java
@@ -2,10 +2,8 @@ package com.thebluehats.server.game.managers.enchants;
 
 import com.thebluehats.server.game.enchants.processedevents.PostEventTemplateResult;
 
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public interface DamageEnchant extends CustomEnchant, PostEventExecutor<PostEventTemplateResult> {
-    @EventHandler
     void onEntityDamageEntity(EntityDamageByEntityEvent event);
 }
