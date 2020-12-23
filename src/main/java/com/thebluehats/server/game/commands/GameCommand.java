ea9648c3e22fec4e09af7ce338c1f764460a34c6
Im-Stevemmmmm <estebangarcia2121@gmail.com>
2020-12-23 14:12:53 -0800

    Reduced boilerplate in commands

--- src/main/java/com/thebluehats/server/game/commands/GameCommand.java
+++ src/main/java/com/thebluehats/server/game/commands/GameCommand.java
@@ -3,25 +3,53 @@ package com.thebluehats.server.game.commands;
 import java.util.StringJoiner;
 
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public abstract class GameCommand implements CommandExecutor {
-    public void sendUsageMessage(Player player, String cmd, String description, String... args) {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        String commandName = getCommandName();
+
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+
+            if (label.equalsIgnoreCase(commandName)) {
+                if (args.length == 0) {
+                    player.sendMessage(getUsageMessage(commandName));
+
+                    return true;
+                }
+            } else {
+                runCommand(player, args);
+            }
+        } else {
+            sender.sendMessage("Only players may run this command.");
+        }
+
+        return true;
+    }
+
+    public String formatUsageMessage(String cmd, String description, String... args) {
         StringJoiner argsJoiner = new StringJoiner(" ");
 
         for (String arg : args) {
             argsJoiner.add("<" + arg + ">");
         }
 
-        String usageMessage = ChatColor.DARK_PURPLE + "/" + cmd + " - " + ChatColor.RED + description
-                + ChatColor.DARK_PURPLE + " | Usage" + ChatColor.DARK_PURPLE + "/" + cmd + " " + ChatColor.RED
-                + argsJoiner.toString();
-
-        player.sendMessage(usageMessage);
+        return ChatColor.DARK_PURPLE + "/" + cmd + " - " + ChatColor.RED + description + ChatColor.DARK_PURPLE
+                + " | Usage" + ChatColor.DARK_PURPLE + "/" + cmd + " " + ChatColor.RED + argsJoiner.toString();
     }
 
-    public void sendErrorMessage(Player player, String error) {
-        player.sendMessage(ChatColor.DARK_PURPLE + "Error! " + ChatColor.RED + error);
+    public String formatErrorMessage(String error) {
+        return ChatColor.DARK_PURPLE + "Error! " + ChatColor.RED + error;
     }
+
+    public abstract String getCommandName();
+
+    public abstract String getUsageMessage(String cmd);
+
+    public abstract void runCommand(Player player, String[] args);
 }
--- src/test/java/com/thebluehats/server/game/commands/GameCommandTest.java
+++ src/test/java/com/thebluehats/server/game/commands/GameCommandTest.java
@@ -1,50 +1,56 @@
 package com.thebluehats.server.game.commands;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.junit.Assert.assertEquals;
 
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.junit.Test;
 
 public class GameCommandTest {
     @Test
-    public void SendsTheProperUsageMessage() {
+    public void FormatsTheUsageMessageCorrectly() {
         GameCommand gameCommand = new GameCommand() {
-            @Override
-            public boolean onCommand(CommandSender commandSender, Command cmd, String label, String[] args) {
-                return true;
+            public String getCommandName() {
+                return "";
+            }
+
+            public String getUsageMessage(String cmd) {
+                return "";
+            }
+
+            public void runCommand(Player player, String[] args) {
             }
         };
 
-        Player player = mock(Player.class);
-
-        gameCommand.sendUsageMessage(player, "give", "gives an item", "player", "item", "amount");
+        String formattedUsageMessage = gameCommand.formatUsageMessage("give", "gives an item", "player", "item",
+                "amount");
 
         String expectedMessage = ChatColor.DARK_PURPLE + "/give - " + ChatColor.RED + "gives an item"
                 + ChatColor.DARK_PURPLE + " | Usage" + ChatColor.DARK_PURPLE + "/give " + ChatColor.RED
                 + "<player> <item> <amount>";
 
-        verify(player).sendMessage(expectedMessage);
+        assertEquals(expectedMessage, formattedUsageMessage);
     }
 
     @Test
-    public void SendsTheProperErrorMessage() {
+    public void FormatsTheErrorMessageCorrectly() {
         GameCommand gameCommand = new GameCommand() {
-            @Override
-            public boolean onCommand(CommandSender commandSender, Command cmd, String label, String[] args) {
-                return true;
+            public String getCommandName() {
+                return "";
+            }
+
+            public String getUsageMessage(String cmd) {
+                return "";
+            }
+
+            public void runCommand(Player player, String[] args) {
             }
         };
 
-        Player player = mock(Player.class);
-
-        gameCommand.sendErrorMessage(player, "Something happend!");
+        String formattedErrorMessage = gameCommand.formatErrorMessage("Something happend!");
 
         String expectedMessage = ChatColor.DARK_PURPLE + "Error! " + ChatColor.RED + "Something happend!";
 
-        verify(player).sendMessage(expectedMessage);
+        assertEquals(expectedMessage, formattedErrorMessage);
     }
 }
