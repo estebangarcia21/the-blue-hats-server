8060e39175f47e2d53a3d980bd1c29077792a500
Haberno <deathersmc@gmail.com>
2020-12-23 20:42:56 -0600

    Added Diamond Allergy

---
+++ src/main/java/com/thebluehats/server/game/enchants/DiamondAllergy.java
@@ -0,0 +1,87 @@
+package com.thebluehats.server.game.enchants;
+
+import java.util.ArrayList;
+
+import com.google.inject.Inject;
+import com.thebluehats.server.game.enchants.processedevents.PostEventTemplateResult;
+import com.thebluehats.server.game.managers.combat.DamageManager;
+import com.thebluehats.server.game.managers.combat.templates.PlayerHitPlayerTemplate;
+import com.thebluehats.server.game.managers.combat.templates.TargetPlayer;
+import com.thebluehats.server.game.managers.enchants.DamageEnchant;
+import com.thebluehats.server.game.managers.enchants.EnchantGroup;
+import com.thebluehats.server.game.managers.enchants.EnchantProperty;
+import com.thebluehats.server.game.utils.EnchantLoreParser;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public class DiamondAllergy implements DamageEnchant {
+    private final EnchantProperty<Float> damageReduction = new EnchantProperty<>(0.10f, 0.20f, 0.30f);
+
+    private final DamageManager damageManager;
+    private final PlayerHitPlayerTemplate playerHitPlayerTemplate;
+
+    @Inject
+    public DiamondAllergy(DamageManager damageManager, PlayerHitPlayerTemplate playerHitPlayerTemplate) {
+        this.damageManager = damageManager;
+        this.playerHitPlayerTemplate = playerHitPlayerTemplate;
+    }
+
+    @Override
+    public void onEntityDamageEntity(EntityDamageByEntityEvent event) {
+        playerHitPlayerTemplate.run(this, event, TargetPlayer.DAMAGEE);
+    }
+
+    @Override
+    public void execute(PostEventTemplateResult data) {
+        EntityDamageByEntityEvent event = data.getEvent();
+        Player damager = data.getDamager();
+        int level = data.getPrimaryLevel();
+
+        if (damager.getInventory().getItemInMainHand().getType() == Material.DIAMOND_SWORD) {
+            damageManager.reduceDamage(event, damageReduction.getValueAtLevel(level));
+
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "Diamond Allergy";
+    }
+
+    @Override
+    public String getEnchantReferenceName() {
+        return "Diamondallergy";
+    }
+
+    @Override
+    public ArrayList<String> getDescription(int level) {
+        EnchantLoreParser enchantLoreParser = new EnchantLoreParser(
+                "Recieve <blue>-{0}</blue> damage from<br/> diamond weapons");
+
+        enchantLoreParser.setSingleVariable("10%", "20%", "30%");
+
+        return enchantLoreParser.parseForLevel(level);
+    }
+
+    @Override
+    public boolean isDisabledOnPassiveWorld() {
+        return false;
+    }
+
+    @Override
+    public EnchantGroup getEnchantGroup() {
+        return null;
+    }
+
+    @Override
+    public boolean isRareEnchant() {
+        return false;
+    }
+
+    @Override
+    public Material[] getEnchantItemTypes() {
+        return new Material[] { Material.LEATHER_LEGGINGS };
+    }
+}
