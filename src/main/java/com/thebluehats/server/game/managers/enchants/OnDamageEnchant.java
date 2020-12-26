ea00ddae7d9353e8f42f1b5771a6aeee4c37e275
Im-Stevemmmmm <estebangarcia2121@gmail.com>
2020-12-25 19:09:53 -0800

    Made OnDamageEnchant abstract and added onEvent

--- src/main/java/com/thebluehats/server/game/managers/enchants/OnDamageEnchant.java
+++ src/main/java/com/thebluehats/server/game/managers/enchants/OnDamageEnchant.java
@@ -1,9 +1,34 @@
 package com.thebluehats.server.game.managers.enchants;
 
-import com.thebluehats.server.game.managers.enchants.processedevents.PostDamageEventTemplateResult;
+import com.thebluehats.server.game.managers.combat.templates.PostDamageEventTemplate;
+import com.thebluehats.server.game.managers.combat.templates.EnchantHolder;
+import com.thebluehats.server.game.managers.enchants.processedevents.PostDamageEventResult;
 
+import com.thebluehats.server.game.utils.EntityValidator;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
-public interface OnDamageEnchant extends CustomEnchant, PostEventExecutor<PostDamageEventTemplateResult> {
-    void onEntityDamageEntity(EntityDamageByEntityEvent event);
+public abstract class OnDamageEnchant implements CustomEnchant, PostEventExecutor<EntityDamageByEntityEvent, PostDamageEventResult> {
+    private final PostDamageEventTemplate[] templates;
+    private final EntityValidator[] validators;
+
+    protected OnDamageEnchant(PostDamageEventTemplate[] templates) {
+        this.templates = templates;
+        this.validators = new EntityValidator[0];
+    }
+
+    protected OnDamageEnchant(PostDamageEventTemplate[] templates, EntityValidator[] validators) {
+        this.templates = templates;
+        this.validators = validators;
+    }
+
+    @Override
+    @EventHandler
+    public void onEvent(EntityDamageByEntityEvent event) {
+        for (PostDamageEventTemplate template : templates) {
+            template.run(this, event, getEnchantHolder(), validators);
+        }
+    }
+
+    public abstract EnchantHolder getEnchantHolder();
 }
--- src/main/java/com/thebluehats/server/game/managers/enchants/PostEventExecutor.java
+++ src/main/java/com/thebluehats/server/game/managers/enchants/PostEventExecutor.java
@@ -2,8 +2,11 @@ package com.thebluehats.server.game.managers.enchants;
 
 import com.thebluehats.server.game.managers.enchants.processedevents.TemplateResult;
 
+import org.bukkit.event.Event;
 import org.bukkit.event.Listener;
 
-public interface PostEventExecutor<T extends TemplateResult<?>> extends Listener {
+public interface PostEventExecutor<E extends Event, T extends TemplateResult<?>> extends Listener {
+    void onEvent(E event);
+
     void execute(T data);
 }
