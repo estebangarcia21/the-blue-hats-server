5666d63a672941294850d78fca5ed8ae9cd8143b
Haberno <deathersmc@gmail.com>
2020-12-31 22:51:51 -0600

    Added Pain Focus

---
+++ src/main/java/com/thebluehats/server/game/enchants/PainFocus.java
@@ -0,0 +1,90 @@
+package com.thebluehats.server.game.enchants;
+
+import java.util.ArrayList;
+
+import com.google.inject.Inject;
+import com.thebluehats.server.game.managers.combat.CalculationMode;
+import com.thebluehats.server.game.managers.combat.DamageManager;
+import com.thebluehats.server.game.managers.combat.templates.EnchantHolder;
+import com.thebluehats.server.game.managers.combat.templates.PlayerHitPlayerTemplate;
+import com.thebluehats.server.game.managers.combat.templates.PostDamageEventTemplate;
+import com.thebluehats.server.game.managers.enchants.EnchantGroup;
+import com.thebluehats.server.game.managers.enchants.EnchantProperty;
+import com.thebluehats.server.game.managers.enchants.OnDamageEnchant;
+import com.thebluehats.server.game.managers.enchants.processedevents.PostDamageEventResult;
+import com.thebluehats.server.game.utils.EnchantLoreParser;
+import com.thebluehats.server.game.utils.EntityValidator;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public class PainFocus extends OnDamageEnchant {
+    private final EnchantProperty<Float> damageIncreasePerHeartLost = new EnchantProperty<>(0.01f, 0.02f, 0.05f);
+
+    private final DamageManager damageManager;
+
+    @Inject
+    public PainFocus(DamageManager damageManager, PlayerHitPlayerTemplate playerHitPlayerTemplate) {
+        super(new PostDamageEventTemplate[] { playerHitPlayerTemplate }, new EntityValidator[] { damageManager });
+
+        this.damageManager = damageManager;
+    }
+
+    @Override
+    public void execute(PostDamageEventResult data) {
+        EntityDamageByEntityEvent event = data.getEvent();
+        Player damager = data.getDamager();
+        int level = data.getLevel();
+
+        int heartsLost = (int) (damager.getMaxHealth() - damager.getHealth()) / 2;
+
+        damageManager.addDamage(event, damageIncreasePerHeartLost.getValueAtLevel(level) * heartsLost,
+                CalculationMode.ADDITIVE);
+    }
+
+    @Override
+    public String getName() {
+        return "Pain Focus";
+    }
+
+    @Override
+    public String getEnchantReferenceName() {
+        return "Painfocus";
+    }
+
+    @Override
+    public ArrayList<String> getDescription(int level) {
+        EnchantLoreParser enchantLoreParser = new EnchantLoreParser(
+                "Deal <red>+{0}</red> damage per <red>❤</red><br/> you're missing");
+
+        enchantLoreParser.setSingleVariable("1%", "+2%", "5%");
+
+        return enchantLoreParser.parseForLevel(level);
+    }
+
+    @Override
+    public boolean isDisabledOnPassiveWorld() {
+        return false;
+    }
+
+    @Override
+    public EnchantGroup getEnchantGroup() {
+        return EnchantGroup.A;
+    }
+
+    @Override
+    public boolean isRareEnchant() {
+        return false;
+    }
+
+    @Override
+    public Material[] getEnchantItemTypes() {
+        return new Material[] { Material.GOLD_SWORD };
+    }
+
+    @Override
+    public EnchantHolder getEnchantHolder() {
+        return EnchantHolder.DAMAGER;
+    }
+}
