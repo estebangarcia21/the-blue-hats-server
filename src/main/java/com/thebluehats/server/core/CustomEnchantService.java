36c76a7a9e3ddd41f4e76149de75440d285c366c
Haberno <deathersmc@gmail.com>
2021-01-01 16:44:37 -0600

    Added Perun

--- src/main/java/com/thebluehats/server/core/CustomEnchantService.java
+++ src/main/java/com/thebluehats/server/core/CustomEnchantService.java
@@ -16,16 +16,18 @@ public class CustomEnchantService implements Service {
 
     @Override
     public void provision(Injector injector) {
-        registerer.register(new CustomEnchant[] { injector.getInstance(BeatTheSpammers.class), injector.getInstance(Wasp.class),
-                injector.getInstance(Peroxide.class), injector.getInstance(SprintDrain.class),
-                injector.getInstance(ComboSwift.class), injector.getInstance(ComboDamage.class),
-                injector.getInstance(Healer.class), injector.getInstance(LastStand.class),
-                injector.getInstance(Mirror.class), injector.getInstance(Billionaire.class),
-                injector.getInstance(DiamondStomp.class), injector.getInstance(Punisher.class),
-                injector.getInstance(DiamondAllergy.class), injector.getInstance(Solitude.class),
-                injector.getInstance(Assassin.class), injector.getInstance(Fletching.class),
-                injector.getInstance(KingBuster.class), injector.getInstance(Knockback.class),
-                injector.getInstance(Parasite.class), injector.getInstance(FractionalReserve.class),
-                injector.getInstance(PainFocus.class), injector.getInstance(CriticallyFunky.class) });
+        registerer.register(new CustomEnchant[] { injector.getInstance(BeatTheSpammers.class),
+                injector.getInstance(Wasp.class), injector.getInstance(Peroxide.class),
+                injector.getInstance(SprintDrain.class), injector.getInstance(ComboSwift.class),
+                injector.getInstance(ComboDamage.class), injector.getInstance(Healer.class),
+                injector.getInstance(LastStand.class), injector.getInstance(Mirror.class),
+                injector.getInstance(Billionaire.class), injector.getInstance(DiamondStomp.class),
+                injector.getInstance(Punisher.class), injector.getInstance(DiamondAllergy.class),
+                injector.getInstance(Solitude.class), injector.getInstance(Assassin.class),
+                injector.getInstance(Fletching.class), injector.getInstance(KingBuster.class),
+                injector.getInstance(Knockback.class), injector.getInstance(Parasite.class),
+                injector.getInstance(FractionalReserve.class), injector.getInstance(PainFocus.class),
+                injector.getInstance(CriticallyFunky.class), injector.getInstance(Perun.class) });
+
     }
 }
