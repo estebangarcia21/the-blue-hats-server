53f48ec8d2e4f59a58c21e8507de0de423c2ef40
Haberno <deathersmc@gmail.com>
2021-01-03 17:18:08 -0600

    Added Ring Armor

--- src/main/java/com/thebluehats/server/core/CustomEnchantService.java
+++ src/main/java/com/thebluehats/server/core/CustomEnchantService.java
@@ -21,20 +21,20 @@ public class CustomEnchantService implements Service {
     public void provision(Injector injector) {
         BooBoo booboo = injector.getInstance(BooBoo.class);
 
-        registerer.register(
-                new CustomEnchant[] { injector.getInstance(BeatTheSpammers.class), injector.getInstance(Wasp.class),
-                        injector.getInstance(Peroxide.class), injector.getInstance(SprintDrain.class),
-                        injector.getInstance(ComboSwift.class), injector.getInstance(ComboDamage.class),
-                        injector.getInstance(Healer.class), injector.getInstance(LastStand.class),
-                        injector.getInstance(Mirror.class), injector.getInstance(Billionaire.class),
-                        injector.getInstance(DiamondStomp.class), injector.getInstance(Punisher.class),
-                        injector.getInstance(DiamondAllergy.class), injector.getInstance(Solitude.class),
-                        injector.getInstance(Assassin.class), injector.getInstance(Fletching.class),
-                        injector.getInstance(KingBuster.class), injector.getInstance(Knockback.class),
-                        injector.getInstance(Parasite.class), injector.getInstance(FractionalReserve.class),
-                        injector.getInstance(PainFocus.class), injector.getInstance(CriticallyFunky.class),
-                        injector.getInstance(Bruiser.class), injector.getInstance(BulletTime.class), booboo,
-                        injector.getInstance(ComboHeal.class), injector.getInstance(ComboStun.class) });
+        registerer.register(new CustomEnchant[] { injector.getInstance(BeatTheSpammers.class),
+                injector.getInstance(Wasp.class), injector.getInstance(Peroxide.class),
+                injector.getInstance(SprintDrain.class), injector.getInstance(ComboSwift.class),
+                injector.getInstance(ComboDamage.class), injector.getInstance(Healer.class),
+                injector.getInstance(LastStand.class), injector.getInstance(Mirror.class),
+                injector.getInstance(Billionaire.class), injector.getInstance(DiamondStomp.class),
+                injector.getInstance(Punisher.class), injector.getInstance(DiamondAllergy.class),
+                injector.getInstance(Solitude.class), injector.getInstance(Assassin.class),
+                injector.getInstance(Fletching.class), injector.getInstance(KingBuster.class),
+                injector.getInstance(Knockback.class), injector.getInstance(Parasite.class),
+                injector.getInstance(FractionalReserve.class), injector.getInstance(PainFocus.class),
+                injector.getInstance(CriticallyFunky.class), injector.getInstance(Bruiser.class),
+                injector.getInstance(BulletTime.class), booboo, injector.getInstance(ComboHeal.class),
+                injector.getInstance(ComboStun.class), injector.getInstance(RingArmor.class) });
 
         globalTimer.addListener(booboo);
     }
