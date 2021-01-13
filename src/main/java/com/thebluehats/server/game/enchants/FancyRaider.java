2bc6705a6f658b12f42f771a8e613044b1c58767
Haberno <52333828+Haberno@users.noreply.github.com>
2021-01-12 18:37:53 -0600

    Added Fancy Raider (#41)
    * Added Fancy Raider
    
    * Fixed RingArmor lore
    
    * Removed extra syntax
    
    * Update CustomEnchantService.java
    
    * Fixed extra lore space
    
    Co-authored-by: Esteban Garcia <44991472+Im-Stevemmmmm@users.noreply.github.com>

--- src/main/java/com/thebluehats/server/game/enchants/FancyRaider.java
+++ src/main/java/com/thebluehats/server/game/enchants/FancyRaider.java
@@ -1,5 +1,7 @@
 package com.thebluehats.server.game.enchants;
 
+import java.util.ArrayList;
+
 import com.google.inject.Inject;
 import com.thebluehats.server.game.managers.combat.CalculationMode;
 import com.thebluehats.server.game.managers.combat.DamageManager;
@@ -12,18 +14,17 @@ import com.thebluehats.server.game.managers.enchants.EnchantProperty;
 import com.thebluehats.server.game.managers.enchants.processedevents.DamageEventEnchantData;
 import com.thebluehats.server.game.utils.EnchantLoreParser;
 import com.thebluehats.server.game.utils.EntityValidator;
+
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
-
-public class DiamondStomp extends DamageTriggeredEnchant {
-    private final EnchantProperty<Double> percentDamageIncrease = new EnchantProperty<>(0.7, 0.12, 0.25);
+public class FancyRaider extends DamageTriggeredEnchant {
+    private final EnchantProperty<Float> percentDamageIncrease = new EnchantProperty<>(0.05f, 0.09f, 0.15f);
 
     private final DamageManager damageManager;
 
     @Inject
-    public DiamondStomp(DamageManager damageManager, PlayerDamageTrigger playerDamageTrigger) {
+    public FancyRaider(DamageManager damageManager, PlayerDamageTrigger playerDamageTrigger) {
         super(new DamageEnchantTrigger[] { playerDamageTrigger }, new EntityValidator[] { damageManager });
 
         this.damageManager = damageManager;
@@ -33,33 +34,23 @@ public class DiamondStomp extends DamageTriggeredEnchant {
     public void execute(DamageEventEnchantData data) {
         Player damagee = data.getDamagee();
 
-        if (playerHasDiamondPiece(damagee)) {
+        if (playerHasLeatherPiece(damagee)) {
             damageManager.addDamage(data.getEvent(), percentDamageIncrease.getValueAtLevel(data.getLevel()),
                     CalculationMode.ADDITIVE);
         }
     }
 
-    private boolean playerHasDiamondPiece(Player player) {
+    private boolean playerHasLeatherPiece(Player player) {
         if (player.getInventory().getHelmet() != null) {
-            if (player.getInventory().getHelmet().getType() == Material.DIAMOND_HELMET) {
-                return true;
-            }
-        }
-
-        if (player.getInventory().getChestplate() != null) {
-            if (player.getInventory().getHelmet().getType() == Material.DIAMOND_CHESTPLATE) {
+            if (player.getInventory().getHelmet().getType() == Material.LEATHER_HELMET) {
                 return true;
             }
         }
 
         if (player.getInventory().getLeggings() != null) {
-            if (player.getInventory().getLeggings().getType() == Material.DIAMOND_LEGGINGS) {
+            if (player.getInventory().getLeggings().getType() == Material.LEATHER_LEGGINGS) {
                 return true;
             }
-        }
-
-        if (player.getInventory().getBoots() != null) {
-            return player.getInventory().getLeggings().getType() == Material.DIAMOND_LEGGINGS;
         }
 
         return false;
@@ -67,20 +58,20 @@ public class DiamondStomp extends DamageTriggeredEnchant {
 
     @Override
     public String getName() {
-        return "Diamond Stomp";
+        return "Fancy Raider";
     }
 
     @Override
     public String getEnchantReferenceName() {
-        return "Diamondstomp";
+        return "Fancyraider";
     }
 
     @Override
     public ArrayList<String> getDescription(int level) {
         EnchantLoreParser enchantLoreParser = new EnchantLoreParser(
-                "Deal <red>+{0}</red> damage vs. players<br/>wearing diamond armor");
+                "Deal <red>+</red> damage vs. players<br/>wearing leather armor");
 
-        enchantLoreParser.setSingleVariable("7%", "12%", "25%");
+        enchantLoreParser.setSingleVariable("5%", "9%", "15%");
 
         return enchantLoreParser.parseForLevel(level);
     }
@@ -92,6 +83,7 @@ public class DiamondStomp extends DamageTriggeredEnchant {
 
     @Override
     public EnchantGroup getEnchantGroup() {
+        // TODO Determine Enchant Group
         return EnchantGroup.A;
     }
 
