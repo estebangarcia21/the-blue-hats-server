493f9dc27b0b8befcc99713dcbee0fc20899669c
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-13 14:53:43 -0800

    Fixed telebow proccing without crouching

--- src/main/java/com/thebluehats/server/game/enchants/Telebow.java
+++ src/main/java/com/thebluehats/server/game/enchants/Telebow.java
@@ -11,9 +11,9 @@ import org.bukkit.Sound;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -23,6 +23,7 @@ import java.util.UUID;
 
 public class Telebow implements CustomEnchant, Listener {
     private final EnchantProperty<Integer> cooldownTimes = new EnchantProperty<>(90, 45, 20);
+    private final ArrayList<UUID> crouchingPlayers = new ArrayList<>();
 
     private final RegionManager regionManager;
     private final CustomEnchantUtils customEnchantUtils;
@@ -36,17 +37,36 @@ public class Telebow implements CustomEnchant, Listener {
     }
 
     @EventHandler
+    public void onBowShoot(EntityShootBowEvent event) {
+        if (event.getEntity() instanceof Player && event.getProjectile() instanceof Arrow) {
+            Player player = (Player) event.getEntity();
+            Arrow arrow = (Arrow) event.getProjectile();
+
+            ItemStack bow = player.getInventory().getItemInHand();
+
+            if (customEnchantUtils.itemHasEnchant(this, bow)) {
+                if (player.isSneaking()) {
+                    crouchingPlayers.add(player.getUniqueId());
+                }
+            }
+        }
+    }
+
+    @EventHandler
     public void onArrowHit(ProjectileHitEvent event) {
         if (event.getEntity() instanceof Arrow) {
             Arrow projectile = (Arrow) event.getEntity();
 
             if (projectile.getShooter() instanceof Player) {
                 Player shooter = (Player) event.getEntity().getShooter();
+                UUID shooterUuid = shooter.getUniqueId();
 
                 ItemStack bow = shooter.getInventory().getItemInHand();
 
                 if (timer.isRunning(shooter.getUniqueId())) {
                     sendCooldownMessage(shooter);
+
+                    crouchingPlayers.remove(shooterUuid);
 
                     return;
                 }
@@ -61,9 +81,11 @@ public class Telebow implements CustomEnchant, Listener {
     public void execute(int level, Player player, Arrow arrow) {
         UUID playerUuid = player.getUniqueId();
 
-        if (!timer.isRunning(playerUuid) && !regionManager.entityIsInSpawn(arrow)) {
+        if (!timer.isRunning(playerUuid) && !regionManager.entityIsInSpawn(arrow) && crouchingPlayers.contains(playerUuid)) {
             player.teleport(arrow);
             player.getWorld().playSound(arrow.getLocation(), Sound.ENDERMAN_TELEPORT, 1f, 2f);
+
+            crouchingPlayers.remove(playerUuid);
         }
 
         timer.start(playerUuid, cooldownTimes.getValueAtLevel(level) * 20, true);
