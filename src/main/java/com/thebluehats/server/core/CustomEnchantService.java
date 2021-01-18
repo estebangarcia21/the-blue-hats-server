ea92c147722ad93bb1c81feb892e9a638eedcc92
Haberno <52333828+Haberno@users.noreply.github.com>
2021-01-17 22:38:25 -0600

    Add Gamble (#48)
    * Added Gamble
    
    * Added Gamble Lore
    
    * Removed redundant if statement
    
    Co-authored-by: Esteban Garcia <44991472+Im-Stevemmmmm@users.noreply.github.com>

--- src/main/java/com/thebluehats/server/core/CustomEnchantService.java
+++ src/main/java/com/thebluehats/server/core/CustomEnchantService.java
@@ -21,29 +21,28 @@ public class CustomEnchantService implements Service {
     public void provision(Injector injector) {
         BooBoo booboo = injector.getInstance(BooBoo.class);
         DoubleJump doubleJump = injector.getInstance(DoubleJump.class);
-
-        registerer.register(
-                new CustomEnchant[] { injector.getInstance(BeatTheSpammers.class), injector.getInstance(Wasp.class),
-                        injector.getInstance(Peroxide.class), injector.getInstance(SprintDrain.class),
-                        injector.getInstance(ComboSwift.class), injector.getInstance(ComboDamage.class),
-                        injector.getInstance(Healer.class), injector.getInstance(LastStand.class),
-                        injector.getInstance(Mirror.class), injector.getInstance(Billionaire.class),
-                        injector.getInstance(DiamondStomp.class), injector.getInstance(Punisher.class),
-                        injector.getInstance(DiamondAllergy.class), injector.getInstance(Solitude.class),
-                        injector.getInstance(Assassin.class), injector.getInstance(Fletching.class),
-                        injector.getInstance(KingBuster.class), injector.getInstance(Knockback.class),
-                        injector.getInstance(Parasite.class), injector.getInstance(FractionalReserve.class),
-                        injector.getInstance(PainFocus.class), injector.getInstance(CriticallyFunky.class),
-                        injector.getInstance(Bruiser.class), injector.getInstance(BulletTime.class), booboo,
-                        injector.getInstance(ComboHeal.class), injector.getInstance(ComboStun.class),
-                        injector.getInstance(DevilChicks.class), injector.getInstance(Perun.class),
-                        injector.getInstance(Crush.class), injector.getInstance(RingArmor.class),
-                        injector.getInstance(FancyRaider.class), injector.getInstance(PushComesToShove.class),
-                        injector.getInstance(Volley.class), injector.getInstance(Explosive.class),
-                        injector.getInstance(Telebow.class), injector.getInstance(Robinhood.class),
-                        injector.getInstance(MegaLongBow.class), injector.getInstance(LuckyShot.class), doubleJump,
-                        injector.getInstance(Chipping.class), injector.getInstance(Shark.class),
-                        injector.getInstance(Chipping.class), injector.getInstance(SpeedyHit.class), });
+      
+        registerer.register(new CustomEnchant[] { injector.getInstance(BeatTheSpammers.class),
+                injector.getInstance(Wasp.class), injector.getInstance(Peroxide.class),
+                injector.getInstance(SprintDrain.class), injector.getInstance(ComboSwift.class),
+                injector.getInstance(ComboDamage.class), injector.getInstance(Healer.class),
+                injector.getInstance(LastStand.class), injector.getInstance(Mirror.class),
+                injector.getInstance(Billionaire.class), injector.getInstance(DiamondStomp.class),
+                injector.getInstance(Punisher.class), injector.getInstance(DiamondAllergy.class),
+                injector.getInstance(Solitude.class), injector.getInstance(Assassin.class),
+                injector.getInstance(Fletching.class), injector.getInstance(KingBuster.class),
+                injector.getInstance(Knockback.class), injector.getInstance(Parasite.class),
+                injector.getInstance(FractionalReserve.class), injector.getInstance(PainFocus.class),
+                injector.getInstance(CriticallyFunky.class), injector.getInstance(Bruiser.class),
+                injector.getInstance(BulletTime.class), booboo, injector.getInstance(ComboHeal.class),
+                injector.getInstance(ComboStun.class), injector.getInstance(DevilChicks.class),
+                injector.getInstance(Perun.class), injector.getInstance(Crush.class),
+                injector.getInstance(RingArmor.class), injector.getInstance(FancyRaider.class),
+                injector.getInstance(PushComesToShove.class), injector.getInstance(Volley.class),
+                injector.getInstance(Explosive.class), injector.getInstance(Telebow.class),
+                injector.getInstance(Robinhood.class), injector.getInstance(MegaLongBow.class),
+                injector.getInstance(LuckyShot.class), doubleJump, injector.getInstance(Gamble.class), 
+                injector.getInstance(Chipping.class) });
 
         globalTimer.addListener(booboo);
         globalTimer.addListener(doubleJump);
