4f307f91fa5dee9a27846f451d33e8ee61bbf76e
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-18 19:16:05 -0800

    Removed APIListener

--- src/main/java/com/thebluehats/server/api/listeners/RegisterPlayer.java
+++ src/main/java/com/thebluehats/server/api/listeners/RegisterPlayer.java
@@ -3,11 +3,12 @@ package com.thebluehats.server.api.listeners;
 import com.thebluehats.server.core.modules.annotations.ServerAPI;
 import kong.unirest.UnirestInstance;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import javax.inject.Inject;
 
-public class RegisterPlayer extends APIListener {
+public class RegisterPlayer implements Listener {
     private final UnirestInstance serverAPI;
 
     @Inject
@@ -17,7 +18,7 @@ public class RegisterPlayer extends APIListener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-         standardRequest(serverAPI.post("/api/v1/players"))
+         serverAPI.post("/api/v1/players")
             .header("Content-Type", "application/json")
             .body("{\n    \"uuid\": \"" + event.getPlayer().getUniqueId() + "\"\n}\n")
             .asJson();
