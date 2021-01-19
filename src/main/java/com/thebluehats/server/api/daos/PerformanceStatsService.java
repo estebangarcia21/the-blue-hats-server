2c4c47b93dc4fac5830ed3572c47853a426669c6
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-18 19:16:14 -0800

    Renamed to PerformanceStatsService

--- src/main/java/com/thebluehats/server/api/daos/PerformanceStatsService.java
+++ src/main/java/com/thebluehats/server/api/daos/PerformanceStatsService.java
@@ -2,7 +2,7 @@ package com.thebluehats.server.api.daos;
 
 import org.bukkit.entity.Player;
 
-public interface PitDataDao {
+public interface PerformanceStatsService {
     int getPlayerXp(Player player);
 
     void setPlayerXp(Player player, int value);
