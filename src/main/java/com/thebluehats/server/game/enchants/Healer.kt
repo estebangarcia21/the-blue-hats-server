99d62f62cd74dc426a2fb5bbb0d0385b2029f430
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-21 10:01:21 -0800

    Added player health utility

--- src/main/java/com/thebluehats/server/game/enchants/Healer.kt
+++ src/main/java/com/thebluehats/server/game/enchants/Healer.kt
@@ -8,53 +8,36 @@ import com.thebluehats.server.game.managers.enchants.EnchantGroup
 import com.thebluehats.server.game.managers.enchants.EnchantProperty
 import com.thebluehats.server.game.managers.enchants.processedevents.DamageEventEnchantData
 import com.thebluehats.server.game.utils.EnchantLoreParser
+import com.thebluehats.server.game.utils.addHealth
 import org.bukkit.Material
 import java.util.*
 
 class Healer @Inject constructor(playerDamageTrigger: PlayerDamageTrigger) :
     DamageTriggeredEnchant(arrayOf(playerDamageTrigger)) {
-    private val healAmount = EnchantProperty(2, 4, 6)
+    private val healAmount = EnchantProperty(2.0, 4.0, 6.0)
+
+    override val enchantHolder: EnchantHolder get() = EnchantHolder.DAMAGER
+    override val name: String get() = "Healer"
+    override val enchantReferenceName: String get() = "Healer"
+    override val isDisabledOnPassiveWorld: Boolean get() = false
+    override val enchantGroup: EnchantGroup get() = EnchantGroup.B
+    override val isRareEnchant: Boolean get() = true
+    override val enchantItemTypes: Array<Material> get() = arrayOf(Material.GOLD_SWORD)
+
     override fun execute(data: DamageEventEnchantData) {
-        val damager = data.damager
-        val damaged = data.damagee
         val level = data.level
-        damager.health = Math.min(damager.health + healAmount.getValueAtLevel(level), damager.maxHealth)
-        damaged.health = Math.min(damaged.health + healAmount.getValueAtLevel(level), damaged.maxHealth)
-    }
 
-    override fun getName(): String {
-        return "Healer"
-    }
-
-    override fun getEnchantReferenceName(): String {
-        return "Healer"
+        data.damager addHealth healAmount.getValueAtLevel(level)
+        data.damagee addHealth healAmount.getValueAtLevel(level)
     }
 
     override fun getDescription(level: Int): ArrayList<String> {
         val enchantLoreParser = EnchantLoreParser(
             "Hitting a player <green>heals</green> both you and them for <red>{0}</red>"
         )
+
         enchantLoreParser.setSingleVariable("1❤", "2❤", "3❤")
+
         return enchantLoreParser.parseForLevel(level)
-    }
-
-    override fun isDisabledOnPassiveWorld(): Boolean {
-        return true
-    }
-
-    override fun getEnchantGroup(): EnchantGroup {
-        return EnchantGroup.B
-    }
-
-    override fun isRareEnchant(): Boolean {
-        return true
-    }
-
-    override fun getEnchantItemTypes(): Array<Material> {
-        return arrayOf(Material.GOLD_SWORD)
-    }
-
-    override fun getEnchantHolder(): EnchantHolder {
-        return EnchantHolder.DAMAGER
     }
 }
---
+++ src/main/java/com/thebluehats/server/game/utils/PlayerHealth.kt
@@ -0,0 +1,15 @@
+package com.thebluehats.server.game.utils
+
+import org.bukkit.entity.Player
+
+infix fun Player.addHealth(value: Double) {
+    health = (health + value).coerceAtMost(maxHealth)
+}
+
+infix fun Player.removeHealth(value: Double) {
+    health = (health - value).coerceAtLeast(0.0)
+}
+
+infix fun Player.setHealth(value: Double) {
+    health = value.coerceIn(0.0, maxHealth)
+}
