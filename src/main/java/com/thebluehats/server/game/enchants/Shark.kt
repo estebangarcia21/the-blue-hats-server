fb0e90a2acaa6fb7d4b94e0ec323d20fdf769316
Haberno <deathersmc@gmail.com>
2021-01-21 17:21:31 -0600

    Converted Shark to Kotlin

--- src/main/java/com/thebluehats/server/game/enchants/Shark.kt
+++ src/main/java/com/thebluehats/server/game/enchants/Shark.kt
@@ -3,7 +3,6 @@ package com.thebluehats.server.game.enchants
 import com.google.inject.Inject
 import com.thebluehats.server.game.managers.combat.CalculationMode
 import com.thebluehats.server.game.managers.combat.DamageManager
-import com.thebluehats.server.game.managers.combat.templates.DamageEnchantTrigger
 import com.thebluehats.server.game.managers.combat.templates.EnchantHolder
 import com.thebluehats.server.game.managers.combat.templates.PlayerDamageTrigger
 import com.thebluehats.server.game.managers.enchants.DamageTriggeredEnchant
@@ -11,18 +10,26 @@ import com.thebluehats.server.game.managers.enchants.EnchantGroup
 import com.thebluehats.server.game.managers.enchants.EnchantProperty
 import com.thebluehats.server.game.managers.enchants.processedevents.DamageEventEnchantData
 import com.thebluehats.server.game.utils.EnchantLoreParser
-import com.thebluehats.server.game.utils.EntityValidator
 import org.bukkit.Material
 import org.bukkit.entity.Player
 import java.util.*
 
 class Shark @Inject constructor(private val damageManager: DamageManager, playerDamageTrigger: PlayerDamageTrigger) :
     DamageTriggeredEnchant(
-        arrayOf<DamageEnchantTrigger>(playerDamageTrigger), arrayOf<EntityValidator>(
+        arrayOf(playerDamageTrigger), arrayOf(
             damageManager
         )
     ) {
     private val percentDamageIncrease = EnchantProperty(0.02f, 0.04f, 0.07f)
+
+    override val name: String get() = "Shark"
+    override val enchantReferenceName: String get() = "Shark"
+    override val isDisabledOnPassiveWorld: Boolean get() = false
+    override val enchantGroup: EnchantGroup get() = EnchantGroup.B
+    override val isRareEnchant: Boolean get() = false
+    override val enchantItemTypes: Array<Material> get() = arrayOf(Material.GOLD_SWORD)
+    override val enchantHolder: EnchantHolder get() = EnchantHolder.DAMAGER
+
     override fun execute(data: DamageEventEnchantData) {
         val event = data.event
         val damager = data.damager
@@ -42,40 +49,11 @@ class Shark @Inject constructor(private val damageManager: DamageManager, player
         )
     }
 
-    override fun getName(): String {
-        return "Shark"
-    }
-
-    override fun getEnchantReferenceName(): String {
-        return "Shark"
-    }
-
     override fun getDescription(level: Int): ArrayList<String> {
         val enchantLoreParser = EnchantLoreParser(
             "Deal <red>+{0}</red> damage per other<br/>player below <red>6❤</red> within 12<br/>blocks"
         )
         enchantLoreParser.setSingleVariable("2%", "4%", "7%")
         return enchantLoreParser.parseForLevel(level)
-    }
-
-    override fun isDisabledOnPassiveWorld(): Boolean {
-        return false
-    }
-
-    override fun getEnchantGroup(): EnchantGroup {
-        // TODO Determine EnchantGroup
-        return EnchantGroup.B
-    }
-
-    override fun isRareEnchant(): Boolean {
-        return false
-    }
-
-    override fun getEnchantItemTypes(): Array<Material> {
-        return arrayOf(Material.GOLD_SWORD)
-    }
-
-    override fun getEnchantHolder(): EnchantHolder {
-        return EnchantHolder.DAMAGER
     }
 }
