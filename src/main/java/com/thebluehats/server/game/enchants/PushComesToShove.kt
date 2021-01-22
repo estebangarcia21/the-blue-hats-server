a0d416158221d12b50121bcb58df0f1dc921dcfe
Haberno <52333828+Haberno@users.noreply.github.com>
2021-01-21 19:06:32 -0600

    Converted PushComesToShove to Kotlin (#79)

--- src/main/java/com/thebluehats/server/game/enchants/PushComesToShove.kt
+++ src/main/java/com/thebluehats/server/game/enchants/PushComesToShove.kt
@@ -15,9 +15,18 @@ import java.util.*
 import javax.inject.Inject
 
 class PushComesToShove @Inject constructor(private val hitCounter: HitCounter, arrowDamageTrigger: ArrowDamageTrigger) :
-    DamageTriggeredEnchant(arrayOf<DamageEnchantTrigger>(arrowDamageTrigger)) {
+    DamageTriggeredEnchant(arrayOf(arrowDamageTrigger)) {
     private val pctsForce = EnchantProperty(12f, 25f, 35f)
     private val damageAmount = EnchantProperty(0f, 1f, 2f)
+
+    override val name: String get() = "Push Comes to Shove"
+    override val enchantReferenceName: String get() = "Pcts"
+    override val isDisabledOnPassiveWorld: Boolean get() = false
+    override val enchantGroup: EnchantGroup get() = EnchantGroup.B
+    override val isRareEnchant: Boolean get() = false
+    override val enchantItemTypes: Array<Material> get() = arrayOf(Material.BOW)
+    override val enchantHolder: EnchantHolder get() = EnchantHolder.DAMAGER
+
     override fun execute(data: DamageEventEnchantData) {
         val damagee = data.damagee
         val level = data.level
@@ -31,41 +40,14 @@ class PushComesToShove @Inject constructor(private val hitCounter: HitCounter, a
         }
     }
 
-    override fun getName(): String {
-        return "Push Comes to Shove"
-    }
-
-    override fun getEnchantReferenceName(): String {
-        return "PCTS"
-    }
-
     override fun getDescription(level: Int): ArrayList<String> {
         val enchantLoreParser = EnchantLoreParser("Every 3rd shot on a player<br/><aqua>{0}</aqua>")
         enchantLoreParser.addTextIf(level != 1, " and deals <red>{1}</red><br/>extra damage")
-        val variables: Array<Array<String>> = arrayOfNulls(2)
-        variables[0] = arrayOf("Punch", "Punch V", "Punch VII")
-        variables[1] = arrayOf("", "+0.5❤", "+1❤")
+
+        val variables = varMatrix()
+        variables add Var(0,"Punch", "Punch V", "Punch VII")
+        variables add Var(1,"", "+0.5❤", "+1❤")
         enchantLoreParser.setVariables(variables)
         return enchantLoreParser.parseForLevel(level)
-    }
-
-    override fun isDisabledOnPassiveWorld(): Boolean {
-        return false
-    }
-
-    override fun getEnchantGroup(): EnchantGroup {
-        return EnchantGroup.B
-    }
-
-    override fun isRareEnchant(): Boolean {
-        return false
-    }
-
-    override fun getEnchantItemTypes(): Array<Material> {
-        return arrayOf(Material.BOW)
-    }
-
-    override fun getEnchantHolder(): EnchantHolder {
-        return EnchantHolder.DAMAGER
     }
 }
