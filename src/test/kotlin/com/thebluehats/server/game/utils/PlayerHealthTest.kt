d2b9f47efe0a6c3bcb2f496cfcc8d7313337580b
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-21 17:53:22 -0800

    Fixed unit test assertions

--- src/test/kotlin/com/thebluehats/server/game/utils/PlayerHealthTest.kt
+++ src/test/kotlin/com/thebluehats/server/game/utils/PlayerHealthTest.kt
@@ -7,6 +7,7 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.bukkit.entity.Player
 
 class PlayerHealthTest : DescribeSpec({
@@ -15,45 +16,45 @@ class PlayerHealthTest : DescribeSpec({
             val player = mockk<Player> {
                 every { maxHealth } returns 20.0
                 every { health } returns 5.0
+                every { health = any() } returns Unit
             }
 
             player addHealth 5.0
-            player.health shouldBe 10.0
+            verify { player.health = 10.0 }
 
-
-            player addHealth 12.0
-            player.health shouldBe 20.0
+            player addHealth 24.0
+            verify { player.health = 20.0 }
         }
 
         it("subtracts player health and caps it at 0") {
             val player = mockk<Player> {
                 every { maxHealth } returns 20.0
-                every { health } returns 10.0
+                every { health } returns 5.0
+                every { health = any() } returns Unit
             }
 
-            player removeHealth 5.0
-            player.health shouldBe 5.0
+            player removeHealth 2.0
+            verify { player.health = 3.0 }
 
-
-            player removeHealth 12.0
-            player.health shouldBe 0.0
+            player removeHealth 10.0
+            verify { player.health = 0.0 }
         }
 
         it("sets the player health restraining it between 0 and the max health") {
             val player = mockk<Player> {
                 every { maxHealth } returns 20.0
                 every { health } returns 10.0
+                every { health = any() } returns Unit
             }
 
             player setHealth 5.0
-            player.health shouldBe 5.0
-
+            verify { player.health = 5.0 }
 
             player setHealth 22.0
-            player.health shouldBe 20.0
+            verify { player.health = 20.0 }
 
             player setHealth -3.0
-            player.health shouldBe 0.0
+            verify { player.health = 0.0 }
         }
     }
 })
