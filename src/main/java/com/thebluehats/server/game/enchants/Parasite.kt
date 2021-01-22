514bb6a6a45d3cc974053ea7533b31703d5a73e8
Haberno <52333828+Haberno@users.noreply.github.com>
2021-01-21 19:05:51 -0600

    Converted Parasite to Kotlin (#82)
    * Converted Parasite to Kotlin
    
    * Update Parasite.kt

--- src/main/java/com/thebluehats/server/game/enchants/Parasite.kt
+++ src/main/java/com/thebluehats/server/game/enchants/Parasite.kt
@@ -13,45 +13,26 @@ import org.bukkit.Material
 import java.util.*
 
 class Parasite @Inject constructor(arrowDamageTrigger: ArrowDamageTrigger) :
-    DamageTriggeredEnchant(arrayOf<DamageEnchantTrigger>(arrowDamageTrigger)) {
+    DamageTriggeredEnchant(arrayOf(arrowDamageTrigger)) {
     private val healAmount = EnchantProperty(0.5, 1.0, 2.0)
+
+    override val name: String get() = "Parasite"
+    override val enchantReferenceName: String get() = "Parasite"
+    override val isDisabledOnPassiveWorld: Boolean get() = false
+    override val enchantGroup: EnchantGroup get() = EnchantGroup.B
+    override val isRareEnchant: Boolean get() = false
+    override val enchantItemTypes: Array<Material> get() = arrayOf(Material.BOW)
+    override val enchantHolder: EnchantHolder get() = EnchantHolder.DAMAGER
+
     override fun execute(data: DamageEventEnchantData) {
         val damager = data.damager
         val level = data.level
         damager.health = Math.min(damager.health + healAmount.getValueAtLevel(level), damager.maxHealth)
     }
 
-    override fun getName(): String {
-        return "Parasite"
-    }
-
-    override fun getEnchantReferenceName(): String {
-        return "Parasite"
-    }
-
     override fun getDescription(level: Int): ArrayList<String> {
         val enchantLoreParser = EnchantLoreParser("Heal <red>{0}</red> on arrow hit")
         enchantLoreParser.setSingleVariable("0.25❤", "0.5❤", "1.0❤")
         return enchantLoreParser.parseForLevel(level)
-    }
-
-    override fun isDisabledOnPassiveWorld(): Boolean {
-        return false
-    }
-
-    override fun getEnchantGroup(): EnchantGroup {
-        return EnchantGroup.B
-    }
-
-    override fun isRareEnchant(): Boolean {
-        return false
-    }
-
-    override fun getEnchantItemTypes(): Array<Material> {
-        return arrayOf(Material.BOW)
-    }
-
-    override fun getEnchantHolder(): EnchantHolder {
-        return EnchantHolder.DAMAGER
     }
 }
