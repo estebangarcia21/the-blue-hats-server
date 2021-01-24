259b6a13b59152c60300bc1c034234b6bfb14b7d
Esteban Garcia <estebangarcia2121@gmail.com>
2021-01-23 17:19:27 -0800

    Removed dependencies from settings kts script

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    mavenLocal()
 
     maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots") }
     maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
